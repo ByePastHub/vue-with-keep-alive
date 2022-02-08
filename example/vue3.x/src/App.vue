2f8dbf6b1370be64e333e5bcbf8195e064c9d476
gbye <347904341@qq.com>
2022-02-08 17:13:08 +0800

    feat: 更新dome

--- example/vue3.x/src/App.vue
+++ example/vue3.x/src/App.vue
@@ -2,3 +2,9 @@
   <KeepRouterView />
 </template>
 
+<style>
+* {
+  text-align: center;
+}
+</style>
+
--- example/vue3.x/src/views/Page4.vue
+++ example/vue3.x/src/views/Page4.vue
@@ -3,7 +3,10 @@
     <h1>Page4</h1>
     <input v-model="value" />
     <p></p>
-    <button @click="() => router.replace('/page2')">跳转Page2, router.replace</button>
+    <button @click="() => router.replace('/page2')">跳转Page2, router.replace，如果不要清除缓存，点击我</button>
+    <p></p>
+    <button @click="() => router.reLaunch('/page2')">跳转Page2, router.reLaunch，如果要清除缓存，点击我</button>
+    <p></p>
     <button @click="() => router.push('/page5')">跳转Page5, router.push</button>
   </div>
 </template>
