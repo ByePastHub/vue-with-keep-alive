432d7471742e2bf56cec0f709ba15e8b5fb14b84
gbye <347904341@qq.com>
2022-02-08 17:29:41 +0800

    feat: 更新 BASE

--- example/vue3.x/vite.config.js
+++ example/vue3.x/vite.config.js
@@ -4,6 +4,7 @@ import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-with-keep-alive',
   plugins: [vue(), viteCompression()],
   server: {
     host: '0.0.0.0'
